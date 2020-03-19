bb4e927877d01471f44c315022c5b6c3548b69c9
Aarav Singh <aaravzen@gmail.com>
2020-03-19 15:23:57 -0700

    Added h

--- dw.py
+++ dw.py
@@ -5,6 +5,7 @@ import random
 import asyncio
 import secrets
 
+# Change the following line to change the bot's callword.
 bot = commands.Bot(command_prefix='d ')
 players = []
 teams = []
@@ -102,6 +103,27 @@ async def endgame(ctx):
     for t in teams:
         msg += "%s's team had the following words: %s\n" % (t.get_player_list(), t.get_words())
     await ctx.send(msg)
+
+@bot.command()
+async def h(ctx):
+    msg = """Hi, and welcome to discrypto.
+
+    For a notes cheat sheet, here's a gist: https://gist.github.com/aaravzen/ea17c20a735e7f43c540dccc2d405115
+
+    For instructions on how to play, and to support the official game: https://www.scorpionmasque.com/en/decrypto
+
+    This bot's commands are as follows ('d' is whatever callword you used to get this):
+    - `d reset` - reset the game state
+    - `d addme` - add a player to the game (must be sent by all players who want to be cluegivers)
+    - `d startgame` - make teams and start the game (send out word info to players)
+    - `d draw` - draws a card for a player on each team. Goes in a round robin fashion according to the list sent out to players at the start of the game.
+    - `d reveal` - reveals the last draw for your team (will reveal cards that you didn't draw, will not reveal anything to players not on a team)
+    - `d timer` - starts a one minute timer for the player (for when someone is done writing their clues)
+    `d endgame` - will end the game and send a summary with all the round's words
+
+    If you have any questions or comments, please keep those in mind as you enjoy playing discrypto. Thank you! ~Aarav"""
+    await ctx.send(msg)
+    
 
 
 token = secrets.get_token()
